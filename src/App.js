4aa25f1bee1a3fbef944cb9ffd81f0753c1b470b
erikalbuquerque <eriksilv.77@gmail.com>
2020-07-10 23:44:38 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="main">
+      <header>
+        <h1>Border Radius Previewer</h1>
+      </header>
+
+
+      <div className="content">
+         <div>Box</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
