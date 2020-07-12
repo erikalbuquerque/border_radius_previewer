90dc436daca0777c84ba3f7d52e77a4c17802715
erikalbuquerque <eriksilv.77@gmail.com>
2020-07-12 15:14:50 -0300

    adjustment in box-model css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
-import './index.css';
-import './style-border.css';
+import React, { useState } from "react";
+import "./index.css";
+import "./style-border.css";
 
 function App() {
-  const [ boxText, setBoxText ] = useState('box-model')
-  const [ topLeft, setTopLeft ] = useState(0)
-  const [ topRight, setTopRight ] = useState(0)
-  const [ bottomLeft, setBottomLeft ] = useState(0)
-  const [ bottomRight, setBottomRight ] = useState(0)
+  const [topLeft, setTopLeft] = useState(0);
+  const [topRight, setTopRight] = useState(0);
+  const [bottomLeft, setBottomLeft] = useState(0);
+  const [bottomRight, setBottomRight] = useState(0);
 
-  function borderTopLeft(value){
-    setTopLeft(value)
+  function borderTopLeft(value) {
+    setTopLeft(value);
   }
 
-  function borderTopRight(value){
-    setTopRight(value)
+  function borderTopRight(value) {
+    setTopRight(value);
   }
 
-  function borderBottomLeft(value){
-    setBottomLeft(value)
+  function borderBottomLeft(value) {
+    setBottomLeft(value);
   }
 
-  function borderBottomRight(value){
-    setBottomRight(value)
+  function borderBottomRight(value) {
+    setBottomRight(value);
   }
 
   return (
@@ -32,29 +31,41 @@ function App() {
       </header>
 
       <div className="content">
+        <div className="box">
+          <div className="inputs">
+            <input
+              type="number"
+              value={topLeft}
+              onChange={(e) => borderTopLeft(e.target.value)}
+            />
+            <input
+              type="number"
+              value={bottomLeft}
+              onChange={(e) => borderBottomLeft(e.target.value)}
+            />
+          </div>
 
-         <div className="box">
+          <div className="box-model">
+            <span>
+              border-radius: {topLeft}px {topRight}px {bottomLeft}px {bottomRight}px;
+            </span>
+          </div>
 
-           <div className="inputs">
-             <input type="number" value={topLeft} onChange={(e) => borderTopLeft(e.target.value)}/>
-             <input type="number" value={bottomLeft} onChange={(e) => borderBottomLeft(e.target.value)}/>
-           </div>
+          <div className="inputs">
+            <input
+              type="number"
+              value={topRight}
+              onChange={(e) => borderTopRight(e.target.value)}
+            />
+            <input
+              type="number"
+              value={bottomRight}
+              onChange={(e) => borderBottomRight(e.target.value)}
+            />
+          </div>
+        </div>
 
-           <div className="box-model">
-             <span>{boxText}</span>
-           </div>
-
-           <div className="inputs">
-             <input type="number" value={topRight} onChange={(e) => borderTopRight(e.target.value)}/>
-             <input type="number" value={bottomRight} onChange={(e) => borderBottomRight(e.target.value)}/>
-           </div>
-
-         </div>
-
-          <button>copy css</button>
-
-          <div>TopLeft: {topLeft} TopRigth: {topRight} bottomLeft: {bottomLeft} bottomRight: {bottomRight}</div>
-
+        <button>copy css</button>
       </div>
     </div>
   );
