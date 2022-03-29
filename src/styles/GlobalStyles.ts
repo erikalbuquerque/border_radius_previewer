ce7d6677122cdfade0556f9e3787c4c51d938349
kataik <erik.albuquerque.oficial@gmail>
2022-03-28 22:25:15 -0300

    feat: add global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --white: #ffffff;
+    --brown-700: #423232;
+    --red-400: #ff5733;
+    --green-500: #3db13d;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Nunito', sans-serif;
+  }
+`;
