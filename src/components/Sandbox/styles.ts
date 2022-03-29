426c6f62b3241a74a3d6a2a3d8e129d3316762fe
kataik <erik.albuquerque.oficial@gmail>
2022-03-29 13:48:16 -0300

    fix: align all elements to the left

--- src/components/Sandbox/styles.ts
+++ src/components/Sandbox/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.div({});
 export const Content = styled.div({
   display: "flex",
   flexDirection: "column",
+  alignItems: "flex-start",
   justifyContent: "center",
   gap: "2rem",
 });
