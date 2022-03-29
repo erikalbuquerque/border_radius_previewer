f4ff37ff26ed90eda3e4b02b56884512101a50ce
kataik <erik.albuquerque.oficial@gmail>
2022-03-29 13:46:43 -0300

    fix: max width

--- src/components/PropsDisplay/styles.ts
+++ src/components/PropsDisplay/styles.ts
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
-export const Container = styled.div({});
+export const Container = styled.div({
+  display: "flex",
+  alignItems: "flex-start",
+  width: "347px",
+});
 
 export const Content = styled.div({
   backgroundColor: "var(--cyan)",
   padding: "0.5rem 1rem",
   borderRadius: "0.5rem",
-
   span: {
     color: "var(--brown-700)",
     fontWeight: "700",
