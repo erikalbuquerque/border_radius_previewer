b8f58b591bb72e960e663fb69b53e6961b403275
kataik <erik.albuquerque.oficial@gmail>
2022-03-30 15:16:30 -0300

    fix: resize logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div({
   h1: {
-    fontSize: "3rem",
-    lineHeight: "3rem",
+    fontSize: "2rem",
+    lineHeight: "2rem",
     color: "var(--brown-700)",
   },
 });
